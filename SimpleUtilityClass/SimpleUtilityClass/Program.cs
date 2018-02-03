f9e5bc76c5af6fac7d27f2cc23f2a50c287408ef
Alena <white_fox0@mail.ru>
2018-02-03 14:36:21 +0700

    03.02.2018(2)
    Статические классы.

---
+++ SimpleUtilityClass/SimpleUtilityClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleUtilityClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Static Classes *****\n");
+            //Это работает нормально.
+            TimeUtilClass.PrintDate();
+            TimeUtilClass.PrintTime();
+
+            //Ошибка компиляции! Создавать экземпляр статического класса нельзя!
+            TimeUtilClass u = new TimeUtilClass();
+
+            Console.ReadLine();
+        }
+    }
+}
